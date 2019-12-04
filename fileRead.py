1564d6e1454d04a84fdb8aaecff549d9a1e55732
Michal Laskowski <TJG784@motorolasolutions.com>
2019-12-04 13:52:26 +0100

    spory postep, w zasadzie całe gui

--- fileRead.py
+++ fileRead.py
@@ -1,14 +1,80 @@
 import csv
 
-with open('players_20.csv', encoding='utf8') as data:
-    players = csv.reader(data, delimiter=',')
+class fileRead:
 
-    line_number = 0
-    for a in players:
-        if line_number == 0:
-            column_names = a
-            line_number += 1
-    for b in column_names:
-        print(b)
-        if b == 'overall' || wszystkie pozostale potrzebne kategorie (dla bramkarzy te z gk)(dla reszty normalne z karty)(dla wszystkich overall plus cena):
-            print(b)
+    def __init__(self):
+        with open('players_20.csv', encoding='utf8') as data:
+            players = csv.reader(data, delimiter=',')
+
+            goalkeepers = list()
+            goalkeepers_overalls = list()
+            goalkeepers_values = list()
+            field_players = list()
+            field_players_overalls = list()
+            field_players_values = list()
+
+            row_number = 0
+            column_number = 0
+
+            for row in players:
+                if row_number == 0:
+                    for column in row:
+                        if column == 'player_positions':
+                            player_position = column_number
+                        elif column == 'overall':
+                            overall = column_number
+                        elif column == 'value_eur':
+                            value_eur = column_number
+                        elif column == 'pace':
+                            pace = column_number
+                        elif column == 'shooting':
+                            shooting = column_number
+                        elif column == 'passing':
+                            passing = column_number
+                        elif column == 'dribbling':
+                            dribbling = column_number
+                        elif column == 'defending':
+                            defending = column_number
+                        elif column == 'physic':
+                            physic = column_number
+                        elif column == 'gk_diving':
+                            gk_diving = column_number
+                        elif column == 'gk_handling':
+                            gk_handling = column_number
+                        elif column == 'gk_kicking':
+                            gk_kicking = column_number
+                        elif column == 'gk_reflexes':
+                            gk_reflexes = column_number
+                        elif column == 'gk_speed':
+                            gk_speed = column_number
+                        elif column == 'gk_positioning':
+                            gk_positioning = column_number
+                        column_number += 1
+                else:
+                    player_attributes = []
+                    if row[player_position] == 'GK':
+                        player_attributes.append(int(row[gk_diving]))
+                        player_attributes.append(int(row[gk_handling]))
+                        player_attributes.append(int(row[gk_kicking]))
+                        player_attributes.append(int(row[gk_reflexes]))
+                        player_attributes.append(int(row[gk_speed]))
+                        player_attributes.append(int(row[gk_positioning]))
+                        player_overall = int(row[overall])
+                        player_value = int(row[value_eur])
+                        goalkeepers.append(player_attributes)
+                        goalkeepers_overalls.append(player_overall)
+                        goalkeepers_values.append(player_value)
+                    else:
+                        player_attributes.append(int(row[pace]))
+                        player_attributes.append(int(row[shooting]))
+                        player_attributes.append(int(row[passing]))
+                        player_attributes.append(int(row[dribbling]))
+                        player_attributes.append(int(row[defending]))
+                        player_attributes.append(int(row[physic]))
+                        player_overall = int(row[overall])
+                        player_value = int(row[value_eur])
+                        field_players.append(player_attributes)
+                        field_players_overalls.append(player_overall)
+                        field_players_values.append(player_value)
+
+                row_number += 1
--- gui.py
+++ gui.py
@@ -1,40 +1,108 @@
 import tkinter as tk
+import fileRead
+
 import os
 import sys
 
 class App(tk.Frame):
 
+    def estimate_gk(self):
+        self.gk_diving = int(self.gk_diving_entry.get())
+        self.gk_handling = int(self.gk_handling_entry.get())
+        self.gk_kicking = int(self.gk_kicking_entry.get())
+        self.gk_reflexes = int(self.gk_reflexes_entry.get())
+        self.gk_speed = int(self.gk_speed_entry.get())
+        self.gk_positioning = int(self.gk_positioning_entry.get())
 
-    def get_value(self):
+        print(self.gk_positioning)
 
-        self.first_team = self.first_team_entry.get()
-        self.second_team = self.second_team_entry.get()
+        if ((self.gk_diving > 0 and self.gk_diving < 100) and
+            (self.gk_handling > 0 and self.gk_handling < 100) and
+            (self.gk_kicking > 0 and self.gk_kicking < 100) and
+            (self.gk_reflexes > 0 and self.gk_reflexes < 100) and
+            (self.gk_speed > 0 and self.gk_speed < 100) and
+            (self.gk_positioning > 0 and self.gk_positioning < 100)):
+                pass
+        else:
+            self.new_window = tk.Toplevel(master=self)
+            self.new_window.geometry("300x300")
+            self.new_window.title("ERROR!!!")
 
+            self.error_label = tk.Label(master=self.new_window, text='Entered value must be in range 1 to 99!').grid(row=0, column=0)
+            self.quit_button = tk.Button(master=self.new_window, text='Close window', command=self.new_window.destroy).grid(row=1, column=0)
 
-    def estimate_rate(self):
-        pass
-
-
-    def estimate_price(self):
+    def estimate_fp(self):
         pass
 
     def create_widgets_for_gk(self):
-        pass
+        self.dummy_space3 = tk.Label(text='  ').grid(row=2, column=0)
 
+        self.gk_diving_label = tk.Label(text='Goalkeeper diving: ', height=0, width=20).grid(row=3, column=1)
+        self.gk_diving_entry = tk.Entry()
+        self.gk_diving_entry.grid(row=3, column=2)
+
+        self.gk_handling_label = tk.Label(text='Goalkeeper handling: ', height=0, width=20).grid(row=4, column=1)
+        self.gk_handling_entry = tk.Entry()
+        self.gk_handling_entry.grid(row=4, column=2)
+
+        self.gk_kicking_label = tk.Label(text='Goalkeeper kicking: ', height=0, width=20).grid(row=5, column=1)
+        self.gk_kicking_entry = tk.Entry()
+        self.gk_kicking_entry.grid(row=5, column=2)
+
+        self.gk_reflexes_label = tk.Label(text='Goalkeeper reflexes: ', height=0, width=20).grid(row=6, column=1)
+        self.gk_reflexes_entry = tk.Entry()
+        self.gk_reflexes_entry.grid(row=6, column=2)
+
+        self.gk_speed_label = tk.Label(text='Goalkeeper speed: ', height=0, width=20).grid(row=7, column=1)
+        self.gk_speed_entry = tk.Entry()
+        self.gk_speed_entry.grid(row=7, column=2)
+
+        self.gk_positioning_label = tk.Label(text='Goalkeeper positioning: ', height=0, width=20).grid(row=8, column=1)
+        self.gk_positioning_entry = tk.Entry()
+        self.gk_positioning_entry.grid(row=8, column=2)
+
+        self.estimate_button = tk.Button(text="Estimate!", command=self.estimate_gk, height=0, width=15).grid(row=9,
+                                                                                                              column=2)
+
+    def create_widgets_for_fp(self):
+        self.dummy_space3 = tk.Label(text='  ').grid(row=2, column=0)
+
+        self.fp_pace_label = tk.Label(text='Player pace: ', height=0, width=20).grid(row=3, column=1)
+        self.fp_pace_entry = tk.Entry()
+        self.fp_pace_entry.grid(row=3, column=2)
+
+        self.fp_shooting_label = tk.Label(text='Player shooting: ', height=0, width=20).grid(row=4, column=1)
+        self.fp_shooting_entry = tk.Entry()
+        self.fp_shooting_entry.grid(row=4, column=2)
+
+        self.fp_passing_label = tk.Label(text='Player passing: ', height=0, width=20).grid(row=5, column=1)
+        self.fp_passing_entry = tk.Entry()
+        self.fp_passing_entry.grid(row=5, column=2)
+
+        self.fp_dribbling_label = tk.Label(text='Player dribbling: ', height=0, width=20).grid(row=6, column=1)
+        self.fp_dribbling_entry = tk.Entry()
+        self.fp_dribbling_entry.grid(row=6, column=2)
+
+        self.fp_defending_label = tk.Label(text='Player defending: ', height=0, width=20).grid(row=7, column=1)
+        self.fp_defending_entry = tk.Entry()
+        self.fp_defending_entry.grid(row=7, column=2)
+
+        self.fp_physic_label = tk.Label(text='Player physic: ', height=0, width=20).grid(row=8, column=1)
+        self.fp_physic_entry = tk.Entry()
+        self.fp_physic_entry.grid(row=8, column=2)
+
+        self.estimate_button = tk.Button(text="Estimate!", command=self.estimate_fp, height=0, width=15).grid(row=9,
+                                                                                                              column=2)
 
 
     def create_widgets(self):
-        self.est_rate_buttton = tk.Button(text="Estimate overall rate", command=self.estimate_rate, height=0, width=15).grid(row=0, column=0)
-        self.est_price_buttton = tk.Button(text="Estimate price", command=self.estimate_price, height=0, width=15).grid(row=0, column=1)
+        self.dummy_space1 = tk.Label(text='  ').grid(row=1, column=0)
+        self.dummy_space2 = tk.Label(text='     ').grid(row=1, column=3)
 
-        self.place_label = tk.Label(text="Miejsce").grid(row=1, column=0)
-        self.player_label = tk.Label(text="Gracz").grid(row=1, column=1)
-        self.points_label = tk.Label(text="Punkty").grid(row=1, column=2)
-        self.win_label = tk.Label(text="Wygrane").grid(row=1, column=3)
-        self.draw_label = tk.Label(text="Remisy").grid(row=1, column=4)
-        self.lose_label = tk.Label(text="Przegrane").grid(row=1, column=5)
-        self.goals_ad_label = tk.Label(text="Gole zd.").grid(row=1, column=6)
-        self.goals_lo_label = tk.Label(text="Gole st.").grid(row=1, column=7)
+        self.est_rate_buttton = tk.Button(text="Estimate goalkeeper",
+                                          command=self.create_widgets_for_gk, height=0, width=15).grid(row=1, column=1)
+        self.est_price_buttton = tk.Button(text="Estimate field player",
+                                           command=self.create_widgets_for_fp, height=0, width=15).grid(row=1, column=4)
 
     def ask_quit(self):
         self.root.quit()
@@ -43,6 +111,8 @@ class App(tk.Frame):
         tk.Frame.__init__(self, master)
         self.root = master
         self.root.protocol("WM_DELETE_WINDOW", self.ask_quit)
+
+        self.fifa20 = fileRead.fileRead()
 
         self.create_widgets()
 
